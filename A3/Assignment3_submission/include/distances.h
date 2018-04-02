f0b02b60c43a68fc9489616418beda3cb78ae154
Amro AL-BAALI <aalbaa@ds2.CS.McGill.CA>
2018-04-01 23:12:01 -0400

    I'll remove it here to add the full repo from McGill account

--- A3/Assignment3_submission/include/distances.h
+++
@@ -1,4 +0,0 @@
-#include "preferences.h"
-
-double compute_difference_numeric( PREFERENCE_INFO *one, PREFERENCE_INFO *two );
-double compute_difference_alphabetic( PREFERENCE_INFO *one, PREFERENCE_INFO *two );
